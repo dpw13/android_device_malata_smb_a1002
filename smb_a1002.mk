b0f7dae05050995d4870728ccb1b4e0e29dfff68
Dane Wagner <dane.wagner@gmail.com>
2011-11-27 12:20:05 -0600

    Add working .36 kernel, update fstab, fix some makefile defs
    Change-Id: Ib1fe730acfde92c84dde75fe1072336b9f50135b

--- smb_a1002.mk
+++ smb_a1002.mk
@@ -4,14 +4,15 @@ $(call inherit-product-if-exists, vendor/malata/smb_a1002/smb_a1002-vendor.mk)
 
 # Kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-        LOCAL_KERNEL := device/malata/smb_a1002/kernel
+        LOCAL_KERNEL := device/malata/smb_a1002/kernel.36
 else
         LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/wifi/bcm4329.ko:system/lib/hw/wlan/bcm4329.ko
+    $(LOCAL_PATH)/wifi/bcm4329.36.ko:system/lib/hw/wlan/bcm4329.ko \
+    $(LOCAL_PATH)/init.harmony.rc:root/init.harmony.rc
 
 PRODUCT_PACKAGES += \
     libreference-ril
